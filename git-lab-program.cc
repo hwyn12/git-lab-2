49e9a5927d92269e620757b630e50973d16b5a71
Hannah Wynne <hw457617@ohio.edu>
2019-01-23 19:27:52 -0500

    add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hannah Wynne
+ *        Date: 01/23/19
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
